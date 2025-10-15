917567fe4f32dbfee1718d587e28e8baa1243cda
zakaria <zakariajsp@gmail.com>
2025-10-16 00:00:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,78 @@
+# python banking program
+
+def show_balance(balance):
+    print("**************************************")
+    print(f"Your current balance is: ${balance:.2f}")
+    print("**************************************")
+
+def deposit():
+    print("**************************************")
+    amount = float(input("Enter the amount you want to deposit: "))
+    print("**************************************")
+    
+    if amount < 0:
+        print("**************************************")
+        print("Thats not a valid amount!")
+        print("**************************************")
+
+        return 0
+    else :
+        return amount
+
+def withdraw(balance):
+    print("**************************************")
+    amount = float(input("Enter the amount you want to withdraw: "))
+    print("**************************************")
+
+    if amount > balance:
+        print("**************************************")
+        print("Broke ass nigga!!")
+        print("**************************************")
+
+        return 0
+    elif amount < 0:
+        print("**************************************")
+        print("Please enter a valid amount! ")
+        print("**************************************")
+
+        return 0
+    else:
+        return amount
+def main():
+
+    balance = 0 
+    is_running = True
+
+
+    while is_running:
+        print("**************************************")
+        print("    Welcome to your digital bank")
+        print("**************************************")
+
+        print("1.show balance")
+        print("2.deposit")
+        print("3.withdraw")
+        print("4.exit")
+        print("**************************************")
+
+        choice = input("Enter your choice (1-4): ")
+
+        if choice == "1":
+            show_balance(balance)
+        elif choice == "2":
+            balance += deposit()
+        elif choice == "3":
+            balance -= withdraw(balance)
+        elif choice == "4":
+            is_running = False
+        else :
+            print("**************************************")
+            print("Invalid choice. Please try again.")
+            print("**************************************")
+
+    print("**************************************")
+    print("Thank you for using our digital bank. Goodbye!")
+    print("**************************************")
+
+if __name__ == '__main__':
+    main()
